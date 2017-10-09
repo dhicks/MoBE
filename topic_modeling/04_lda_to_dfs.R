7dfe32d425fdd82a31734dee65aac1eec4870f2d
Dan Hicks <hicks.daniel.j@gmail.com>
2017-10-09 09:13:58 -0700

    first pass at code to coerce LDA output to dataframe

---
+++ topic_modeling/04_lda_to_dfs.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(stringr)
+
+load('../../Eisen-data/03_lda.Rdata')
+
+terms_lda = lda@beta %>%
+    t() %>%
+    as_tibble() %>%
+    rename_all(funs(str_replace(., 'V', 'topic_'))) %>%
+    mutate(term = lda@terms) %>%
+    select(term, everything())
+
+## Currently the document identifiers don't correspond to anything in our metadata
+documents_lda = lda@gamma %>% 
+    as_tibble() %>%
+    rename_all(funs(str_replace(., 'V', 'topic_'))) %>%
+    mutate(document = lda@documents) %>%
+    select(document, everything())
+
+save(terms_lda, documents_lda, file = '../../Eisen-data/04_lda_dfs.Rdata')
