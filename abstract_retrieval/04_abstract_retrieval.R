bb2a5ff67c3209862e26f32987b5e388e4f0509e
Dan Hicks <hicks.daniel.j@gmail.com>
2017-11-06 11:37:46 -0800

    modify abstract retrieval for use w/ much larger list of papers (> 25k)

--- abstract_retrieval/04_abstract_retrieval.R
+++ abstract_retrieval/04_abstract_retrieval.R
@@ -3,19 +3,55 @@ library(RCurl)
 library(xml2)
 library(stringr)
 
+library(foreach)
+library(doSNOW)
+
 source('api_key.R')
 
-load('01_authors_and_papers.Rdata')
+load('../../Eisen-data/03_authors_and_papers.Rdata')
 
-scrape = function (this_sid) {
+## Folder to store XML retrieved from API
+target_folder = '../../Eisen-data/04-abstracts'
+if (!dir.exists(target_folder)) {
+    dir.create(target_folder)
+}
+
+
+## Functions used in the metadata retrieval process
+scrape_ = function (this_sid) {
+    ## Basically just an abstraction of the RCurl call
     base_url = 'https://api.elsevier.com/content/abstract/scopus_id/'
     query_url = str_c(base_url, 
                       this_sid, '?',
                       'apiKey=', api_key)
     
     raw = getURL(query_url)
+    raw
+}
+
+scrape = function (this_sid, target_folder) {
+    ## Either scrape from the API + save the result OR load the saved result
+    target_file_xml = str_c(target_folder, '/', this_sid, '.xml')
+    target_file = str_c(target_folder, '/', this_sid, '.xml.zip')
+    if (!file.exists(target_file)) {
+        raw = scrape_(this_sid)
+        write_file(raw, target_file_xml)
+        zip(target_file, target_file_xml, flags = '-9Xq')
+        unlink(target_file_xml)
+    } else {
+        raw = read_file(target_file)
+    }
+    return(raw)
+}
+
+parse = function (raw) {
     xml = read_xml(raw)
-    xml_ns_strip(xml)
+    xml = xml_ns_strip(xml)
+    
+    scopus_id = xml %>%
+        xml_find_first('//dc:identifier') %>%
+        xml_text() %>%
+        str_extract('[0-9]+')
     
     doi = xml %>%
         xml_find_first('//prism:doi') %>%
@@ -42,52 +78,64 @@ scrape = function (this_sid) {
     auids = author_nodes %>%
         xml_attr('auid') %>%
         list()
-    surnames = author_nodes %>%
-        xml_find_first('ce:surname') %>%
-        xml_text() %>%
-        list()
-    given_names = author_nodes %>%
-        xml_find_first('ce:given-name') %>%
-        xml_text() %>%
-        list()
+    # surnames = author_nodes %>%
+    #     xml_find_first('ce:surname') %>%
+    #     xml_text() %>%
+    #     list()
+    # given_names = author_nodes %>%
+    #     xml_find_first('ce:given-name') %>%
+    #     xml_text() %>%
+    #     list()
     
-    tibble(scopus_id = this_sid, 
-           doi = doi, 
-           title = title, 
-           journal = journal, 
-           date = date, 
-           abstract = abstract, 
-           keywords = keywords, 
-           auids = auids, 
-           surnames = surnames, 
-           given_names = given_names,
-           raw = raw)
+    tibble(scopus_id, doi, title, journal, 
+           date, abstract, keywords, 
+           auids)
 }
 
-abstracts_df = papers_by_auid$scopus_id %>%
-    unique() %>%
-    # head() %>%
-    plyr::ldply(scrape, 
-                .progress = 'text')
+get_metadata = function (this_sid, target_folder) {
+    raw = scrape(this_sid, target_folder)
+    dataf = parse(raw)
+    
+    return(dataf)
+}
 
+
+## Identify papers to retrieve
+papers_to_retrieve = unique(papers_by_auid$scopus_id)
+# papers_to_retrieve = papers_to_retrieve[1:200]
+  
+
+## Run metadata retrieval
+cl = makeCluster(2)
+registerDoSNOW(cl)
+pb = txtProgressBar(max = length(papers_to_retrieve), style = 3)
+progress = function(n) setTxtProgressBar(pb, n)
+system.time(
+abstracts_df <- foreach(paper = papers_to_retrieve, 
+        .combine = bind_rows, 
+        .multicombine = TRUE, 
+        .packages = c('tidyverse', 'RCurl', 'xml2', 'stringr'),
+        .options.snow = list(progress = progress),
+        .verbose = FALSE) %dopar%
+    get_metadata(paper, target_folder)
+)
+stopCluster(cl)
+
+
+## Filter down to auids in our list of authors
 abstracts_df = abstracts_df %>%
-    unnest(auids, surnames, given_names, .drop = FALSE) %>%
-    left_join(unnest(authors), by = 'auids') %>%
-    filter(!is.na(id)) %>%
-    select(-surnames, -given_names) %>%
-    group_by(scopus_id, doi, title, journal, date, 
-             abstract) %>%
-    summarize(keywords = list(first(keywords)),
-              author_ids = list(id),
-              author_groups = list(group),
-              surnames = list(surname),
-              given_names = list(given_name), 
-              auids = list(auids),
-              raw = first(raw))
+    unnest(auids, .drop = FALSE) %>%
+    filter(auids %in% authors$auid) %>% 
+    group_by(scopus_id, doi, title, journal, date) %>%
+    summarize(abstract = first(abstract),
+              keywords = list(first(keywords)),
+              auids = list(auids))
 
-save(abstracts_df, file = '02_abstracts.Rdata')
+
+## Save results
+save(abstracts_df, file = '04_abstracts.Rdata')
 
 abstracts_df %>%
     select(-raw) %>%
     jsonlite::toJSON() %>%
-    write_lines('02_abstracts.json')
+    write_lines('04_abstracts.json')
