ea47a1a1a723d0ae881bd738199f3e36f72e3e65
Dan Hicks <hicks.daniel.j@gmail.com>
2017-11-06 11:37:04 -0800

    big list of papers for all participants w/ >1 paper in the collaboration

--- abstract_retrieval/03_sid_retrieval.R
+++ abstract_retrieval/03_sid_retrieval.R
@@ -5,22 +5,38 @@ library(stringr)
 
 source('api_key.R')
 
-authors = readxl::read_excel('../../Eisen-data/00_author_list.xlsx')
+## Load the Scopus metadata for the Zotero library papers
+load('../../Eisen-data/02_Scopus.Rdata')
 
-authors = authors %>%
-    mutate(auid = str_extract(scopus_page, '[0-9]+')) %>%
-    select(-scopus_page) %>%
-    group_by(group, id, surname, given_name) %>%
-    summarize(auids = list(auid)) %>%
-    ungroup()
+## Reshape scopus_data into one-row-per-auid-paper
+## For now ignore the false duplicates issue:  
+## https://github.com/dhicks/Eisen/issues/1
+authors_unfltd = scopus_data %>%
+    select(-raw) %>%
+    filter(!is.na(sid)) %>%
+    unnest() %>%
+    group_by(auid) %>%
+    summarize(surnames = list(unique(surnames)),
+              given_names = list(unique(given_names)), 
+              n = n()) %>%
+    arrange(desc(n))
 
+## Distribution of paper counts in the Zotero library
+ggplot(authors_unfltd, aes(n)) + 
+    geom_bar()
+## Something like 2/3rds of the authors have only 1 paper
+authors = filter(authors_unfltd, n > 1)
+
+
+## Function to retrieve all* papers for a given auid
+## all* = 200 most recent papers published 2003-2018
 scrape = function (this_auid) {
     base_url = 'https://api.elsevier.com/content/search/scopus?'
     query_url = str_c(base_url, 
                       'query=au-id(', this_auid, ')', '&',
                       'apiKey=', api_key, '&',
                       'count=200', '&',
-                      'date=1998-2018', '&',
+                      'date=2003-2018', '&',
                       'httpAccept=application/xml')
     
     raw = getURL(query_url)
@@ -59,12 +75,11 @@ scrape = function (this_auid) {
            raw = raw)
 }
 
-papers_by_auid = authors$auids %>%
-    unlist() %>%
-    unique() %>%
-    plyr::ldply(scrape, 
-                .progress = 'text')
+papers_by_auid =  plyr::ldply(authors$auid, scrape, .progress = 'text')
 
-save(authors, papers_by_auid, file = '01_authors_and_papers.Rdata')
+papers_raw = select(papers_by_auid, scopus_id, raw)
+papers_by_auid = select(papers_by_auid, -raw)
 
+save(authors, papers_by_auid, file = '../../Eisen-data/03_authors_and_papers.Rdata')
+save(papers_raw, file = '../../Eisen-data/03_papers_raw.Rdata')
 
