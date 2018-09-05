da9b77d63800cfe3ca50695b453a0aecc8b958e3
Dan Hicks <hicks.daniel.j@gmail.com>
2018-09-05 14:05:59 -0700

    tweaks to network analysis that actually happened before previous commits

--- 08_analyze_net.R
+++ 08_analyze_net.R
@@ -3,11 +3,11 @@ library(igraph)
 library(tidygraph)
 library(ggraph)
 
-net = read_rds('07_net_full.Rds')
-net_gc = read_rds('07_net_gc.Rds')
+net = read_rds('../Eisen-data/07_net_full.Rds')
+net_gc = read_rds('../Eisen-data/07_net_gc.Rds')
 
 ## Plot GC over time ----
-## - GC includes almost all authors: 97% as of 18-06-10
+## - GC includes 97% of authors
 net %>%
     activate(nodes) %>%
     as_tibble() %>%
@@ -52,10 +52,25 @@ plots = net_gc %>%
 # plots$plot[[4]]
 cowplot::plot_grid(plotlist = plots$plot)
 ggsave('08_nets.png', height = 6, width = 6)
+cowplot::plot_grid(plotlist = plots$plot, nrow = 1)
+ggsave('08_nets_wide.png', height = 4, width = 12)
 ## Caption:  Evolution of the extended collaboration network over time.  Points are authors; edges are annual publication collections.  Node and edge color distinguish Sloan authors and papers (blue) from authors/papers outside the collaboration (red).  Only authors in the giant component of the full (non-dynamic) network are included; only authors with at least 1 coauthor are included in each panel.  97% of all authors are in the giant component; 99.5% of Sloan collaboration authors are in the giant component.  Network layouts are calculated separately for each panel using the Fruchterman-Reingold algorithm with default parameters. 
 
 
 ## Network statistics over time ----
+## Heterophily over time
+net_gc %>%
+    activate(edges) %>%
+    as_tibble() %>%
+    group_by(year, heterophily) %>%
+    summarize(n = sum(n)) %>%
+    ungroup() %>%
+    spread(heterophily, n) %>%
+    mutate(frac_heterophily = `TRUE` / (`TRUE` + `FALSE`)) %>%
+    ggplot(aes(year, frac_heterophily)) +
+    geom_line() +
+    theme_minimal()
+
 ## These do the same thing; filter down edges, but don't remove any nodes
 # net %>%
 #     activate(edges) %>%
@@ -65,6 +80,8 @@ ggsave('08_nets.png', height = 6, width = 6)
 #     activate(edges) %>%
 #     filter(year <= 2004)
 
+
+## NB whole network, not just GC
 net_by_year = cross_df(list(year = as.integer(unique(E(net)$year)), 
                             sloan_author = c(TRUE, FALSE))) %>%
     arrange(year, sloan_author) %>%
@@ -80,19 +97,20 @@ net_by_year = cross_df(list(year = as.integer(unique(E(net)$year)),
            mean_distance = map_dbl(net, mean_distance), 
            diameter = map_dbl(net, diameter), 
            transitivity = map_dbl(net, transitivity), 
-           density = map_dbl(net, edge_density))
+           density = map_dbl(net, ~ edge_density(simplify(.))))
 
 stat_labels = tribble(
     ~ statistic, ~ pretty_name, 
-    'density', 'density', 
-    'diameter', 'diameter', 
+    'size', 'num. authors', 
+    'n_comp', 'num. components', 
     'gc', 'giant component coverage', 
     'H', 'H', 
+    'diameter', 'diameter', 
     'mean_distance', 'mean distance', 
-    'n_comp', 'num. components', 
-    'size', 'num. authors', 
+    'density', 'density', 
     'transitivity', 'transitivity'
-)
+) %>%
+    mutate(pretty_name = fct_inorder(pretty_name))
 
 # ggplot(net_by_year, aes(year, H, color = sloan_author)) + 
 #     geom_line() + geom_point()
@@ -116,11 +134,13 @@ ggsave('08_net_over_time.png', height = 4, width = 6)
 
 ## Novel collaborations ----
 ## Something like 5/8 of all coauthor dyads have their first pub in the collaboration
-# edges %>%
+# net %>%
+#     activate(edges) %>%
+#     as_tibble() %>%
 #     group_by(auid.x, auid.y, in_collab) %>%
 #     summarize(first_year = min(year)) %>%
 #     spread(in_collab, first_year) %>%
-#     mutate(first_in_collab = is.na(`FALSE`) | (`TRUE` < `FALSE`)) %>% 
+#     mutate(first_in_collab = is.na(`FALSE`) | (`TRUE` < `FALSE`)) %>%
 #     replace_na(list('first_in_collab' = FALSE)) %>%
 #     pull(first_in_collab) %>%
 #     table()
